7baeb4b7105fa06608a14df0b473bfffdfd4d87f
venanciomitidieri <venanciomitidieri@gmail.com>
2020-03-30 16:17:45 -0300

    Add files via upload

---
+++ ex000.py
@@ -0,0 +1,5 @@
+#Faça um programa que exiba a frase "Olá, Mundo"
+
+print('Olá, Mundo')
+
+# Sempre que eu quiser imprimir algo na tela atravez do python devo usar print
---
+++ ex001.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia nome, idade e peso e exiba depois
+
+nome = input('Qual é o seu nome? ')
+idade = input('Qual a sua idade? ')
+peso = input('Qual a sua peso? ')
+
+print('Seu nome é', nome, 'e idade', idade, 'e peso', peso)
